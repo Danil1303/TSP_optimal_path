8ad4fd84da162f48d49f5afcd0781d3606026468
Danil1303 <91407532+Danil1303@users.noreply.github.com>
2023-04-01 13:35:13 +0300

    Добавление функции сброса до значений по умолчанию атрибутов экземпляра класса Ant

--- classes.py
+++ classes.py
@@ -3,14 +3,14 @@ class Point:
     points_dict = {}
     point_number = 1
 
-    def __init__(self, x, y):
+    def __init__(self, x, y) -> None:
         self.point_x = x
         self.point_y = y
         self.points_dict[Point.point_number] = self
         Point.point_number += 1
 
     @classmethod
-    def delete_points(cls):
+    def delete_points(cls) -> None:
         cls.points_dict = {}
         cls.point_number = 1
 
@@ -19,11 +19,15 @@ class Ant:
     __slots__ = ['route', 'total_distance']
     ants_list = []
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.route = [1]
         Ant.ants_list.append(self)
         self.total_distance = 0
 
+    def clear_ant_info(self) -> None:
+        self.route = [1]
+        self.total_distance = 0
+
     @classmethod
-    def delete_ants(cls):
+    def delete_ants(cls) -> None:
         cls.ants_list = []
